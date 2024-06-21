f3d7e0479fb5059c1873212855b785ee9fdc0b8e
sarcaxticlarka <140724625+sarcaxticlarka@users.noreply.github.com>
2024-06-21 17:20:34 +0530

    first commit

---
+++ firstdjango/firstdjango/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    # return HttpResponse("achaa samjh gaya ")
+    return render(request, 'website/index.html')
+
+
+def about(request):
+    return  render(request, 'website/about.html')
+
+def contact(request):
+    return render(request, 'website/contact.html')
